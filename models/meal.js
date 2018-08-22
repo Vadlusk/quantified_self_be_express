dfda01de7d7f646e990bc51308d5d79de2a6d690
VadLusk <AdLusk@gmail.com>
2018-08-21 22:24:22 -0600

    Add all method to meal model.

--- models/meal.js
+++ models/meal.js
@@ -1,5 +1,11 @@
-class Meal {
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
 
+class Meal {
+  static all() {
+    return database('meals').select()
+  }
 }
 
 module.exports = Meal;
