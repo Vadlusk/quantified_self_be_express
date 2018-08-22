d5e13ceaecbc81b9c08bf5ff518f3d427aa130ca
VadLusk <AdLusk@gmail.com>
2018-08-21 22:20:30 -0600

    Add meal model and export to controller.

--- controllers/api/v1/mealsController.js
+++ controllers/api/v1/mealsController.js
@@ -1,5 +1,10 @@
 const Meal = require('../../../models/meal');
 
 const index = (req, res, next) => {
-  Meal.all().then(meal => res.json(meal))
+  Meal.all()
+    .then(meal => res.json(meal))
 };
+
+module.exports = {
+  index
+}
