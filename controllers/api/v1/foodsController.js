6f35c7c2dd3643d135f9539b38d73f0f8120366e
VadLusk <AdLusk@gmail.com>
2018-08-21 21:11:18 -0600

    Add create in controller.

--- controllers/api/v1/foodsController.js
+++ controllers/api/v1/foodsController.js
@@ -1,5 +1,10 @@
 const Food = require('../../../models/food')
 
+const create = (req, res, next) => {
+  Food.create(req.body.food)
+    .then(food => res.json(food))
+}
+
 const index = (req, res, next) => {
   Food.all()
     .then(foods => res.json(foods))
