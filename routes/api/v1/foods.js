82a2de498a9c68690d8d2cf301228dea438e616b
VadLusk <AdLusk@gmail.com>
2018-08-21 19:56:00 -0600

    Add route.

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -3,7 +3,8 @@ var router = express.Router();
 const foodsController = require('../../../controllers/api/v1/foodsController');
 
 router.get('/', foodsController.index);
+router.post('/', foodsController.create);
 router.get('/:id', foodsController.show);
-router.put('/:id', foodsController.update)
+router.put('/:id', foodsController.update);
 
 module.exports = router;
