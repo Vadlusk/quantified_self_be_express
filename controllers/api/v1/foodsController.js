37d5c5ca5e013d3f2497daa1169f31fa5bfe8ba8
VadLusk <AdLusk@gmail.com>
2018-08-21 22:55:57 -0600

    Add destroy method to controller.

--- controllers/api/v1/foodsController.js
+++ controllers/api/v1/foodsController.js
@@ -20,6 +20,10 @@ const update = (req, res, next) => {
     .then(food => sendFood(food[0], res))
 }
 
+const destroy = (req, res, next) => {
+  Food.find(req.params.id) ? Food.destroy(req.params.id) : res.status(404).send
+}
+
 const sendFood = (food, res) => {
   food ? res.json(food) : res.status(404).send
 }
@@ -28,5 +32,6 @@ module.exports = {
   create,
   index,
   show,
-  update
+  update,
+  destroy
 }
