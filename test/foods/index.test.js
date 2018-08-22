a08e268cd4ab6a77bdb491da5a59411e646a77bc
VadLusk <AdLusk@gmail.com>
2018-08-21 22:09:53 -0600

    Shorten response.

--- test/foods/index.test.js
+++ test/foods/index.test.js
@@ -4,17 +4,17 @@ describe('GET /api/v1/foods', () => {
   it('should return all of the foods', done => {
     config.chai.request(config.app)
       .get('/api/v1/foods')
-      .end((err, response) => {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.be.a('array');
-        response.body.length.should.equal(3)
-        response.body[0].should.have.property('id')
-        response.body[0].should.have.property('name')
-        response.body[0].should.have.property('calories')
-        response.body[0].id.should.equal(1)
-        response.body[0].name.should.equal('apple')
-        response.body[0].calories.should.equal(25)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.equal(3)
+        res.body[0].should.have.property('id')
+        res.body[0].should.have.property('name')
+        res.body[0].should.have.property('calories')
+        res.body[0].id.should.equal(1)
+        res.body[0].name.should.equal('apple')
+        res.body[0].calories.should.equal(25)
         done();
    });
  });
