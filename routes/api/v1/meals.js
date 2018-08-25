f0767067d86bbda4f6e2ae1d08cc4d808606bdd4
VadLusk <AdLusk@gmail.com>
2018-08-25 12:04:21 -0600

    Add route.

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -6,5 +6,6 @@ const mealFoodsController = require('../../../controllers/api/v1/mealFoodsContro
 router.get('/', mealsController.index);
 router.get('/:id', mealsController.show);
 router.post('/:mealId/foods/:id', mealFoodsController.create);
+router.delete('/:mealId/foods/:id', mealFoodsController.destroy);
 
 module.exports = router;
