cac91d62af5b31f460ae7df05e96314d042a6ed6
VadLusk <AdLusk@gmail.com>
2018-08-25 14:03:22 -0600

    Add same query for show, add where.

--- models/meal.js
+++ models/meal.js
@@ -5,17 +5,28 @@ const database = require('knex')(configuration);
 class Meal {
   static all() {
     return database.raw(
-      `SELECT meals.id, meals.name,
-        COALESCE(json_agg(foods.* ORDER BY foods.id) FILTER (WHERE foods.id IS NOT NULL), '[]') AS foods
-        FROM meals
-        LEFT JOIN meal_foods ON meals.id = meal_foods.meal_id
-        LEFT JOIN foods ON foods.id = meal_foods.food_id
-        GROUP BY meals.id`
+      `SELECT meals.*,
+       COALESCE(json_agg(foods.* ORDER BY foods.id)
+       FILTER (WHERE foods.id IS NOT NULL), '[]') AS foods
+       FROM meals
+       LEFT JOIN meal_foods ON meals.id = meal_foods.meal_id
+       LEFT JOIN foods ON foods.id = meal_foods.food_id
+       GROUP BY meals.id`
     );
   };
 
   static find(id) {
-    return database('meals').where({id}).first();
+    return database.raw(
+      `SELECT meals.*,
+       COALESCE(json_agg(foods.* ORDER BY foods.id)
+       FILTER (WHERE foods.id IS NOT NULL), '[]') AS foods
+       FROM meals
+       LEFT JOIN meal_foods ON meals.id = meal_foods.meal_id
+       LEFT JOIN foods ON foods.id = meal_foods.food_id
+       WHERE meals.id = ?
+       GROUP BY meals.id`,
+       [id]
+    );
   };
 };
 
