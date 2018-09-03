cab0bfda70f75ced3ada97b910c099e5c53934ef
VadLusk <AdLusk@gmail.com>
2018-09-02 23:06:40 -0600

    Add sad path for create meal food.

--- test/meals/foods/create.test.js
+++ test/meals/foods/create.test.js
@@ -14,4 +14,22 @@ describe('POST /api/v1/meals/:meal_id/foods/:id', () => {
         done();
       });
   });
+  context('should not make a meal food if', () => {
+    it('the meal does not exist', done => {
+      config.chai.request(config.app)
+        .post('/api/v1/meals/5/foods/1')
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+    it('the food does not exist', done => {
+      config.chai.request(config.app)
+        .post('/api/v1/meals/1/foods/99999')
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+  });
 });
