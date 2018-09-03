9acaccdb39a1b77263a8bc43d507c9ffa074b568
VadLusk <AdLusk@gmail.com>
2018-09-02 22:46:02 -0600

    Add 3 sad path tests for create food and fix status for happy path.

--- test/foods/create.test.js
+++ test/foods/create.test.js
@@ -6,7 +6,7 @@ describe('POST /api/v1/foods', () => {
       .post('/api/v1/foods')
       .send({'food': {'name': 'chicken', 'calories': 200}})
       .end((err, response) => {
-        response.should.have.status(200);
+        response.should.have.status(201);
         response.should.be.json;
         response.body.should.be.a('object');
         response.body.should.have.property('id');
@@ -16,5 +16,33 @@ describe('POST /api/v1/foods', () => {
         response.body.calories.should.equal(200);
         done();
       });
+  });
+  context('should not create a food if', () => {
+    it('the body is missing', done => {
+      config.chai.request(config.app)
+        .post('/api/v1/foods')
+        .end((err, response) => {
+          response.should.have.status(400);
+          done();
+        });
+    });
+    it('the name is missing', done => {
+      config.chai.request(config.app)
+        .post('/api/v1/foods')
+        .send({'food': {'name': null, 'calories': 200}})
+        .end((err, response) => {
+          response.should.have.status(400);
+          done();
+        });
+    });
+    it('the calories are missing', done => {
+      config.chai.request(config.app)
+        .post('/api/v1/foods')
+        .send({'food': {'name': 'chicken', 'calories': null}})
+        .end((err, response) => {
+          response.should.have.status(400);
+          done();
+        });
+    });
   });
 });
