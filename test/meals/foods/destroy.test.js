3f4dc38b812409db15ae4f1c06411f83b27af262
VadLusk <AdLusk@gmail.com>
2018-09-03 11:15:12 -0600

    Add two sad path tests for meal food delete.

--- test/meals/foods/destroy.test.js
+++ test/meals/foods/destroy.test.js
@@ -1,7 +1,7 @@
 var config = require('../../testHelper');
 
 describe('DELETE /api/v1/meals/:meal_id/foods/:id', () => {
-  it('should delete a meal_food', done => {
+  it('should delete a meal food', done => {
     let message = 'Successfully removed apple from snack';
 
     config.chai.request(config.app)
@@ -14,5 +14,23 @@ describe('DELETE /api/v1/meals/:meal_id/foods/:id', () => {
         res.body.message.should.equal(message);
         done();
       });
+  });
+  context('should not make a meal food if', () => {
+    it('the meal does not exist', done => {
+      config.chai.request(config.app)
+        .post('/api/v1/meals/5/foods/1')
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+    it('the food does not exist', done => {
+      config.chai.request(config.app)
+        .post('/api/v1/meals/1/foods/99999')
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
   });
 });
