53fdd9f1f2550d5dd9ec3f002b1806be514d844f
VadLusk <AdLusk@gmail.com>
2018-09-03 13:00:29 -0600

    Refactor create meal food with 2 fewer queries.

--- controllers/api/v1/mealFoodsController.js
+++ controllers/api/v1/mealFoodsController.js
@@ -3,24 +3,15 @@ const Food     = require('../../../models/food');
 const Meal     = require('../../../models/meal');
 
 const create = (req, res, next) => {
+  let mealName;
+  let foodName;
   Meal.find(req.params.mealId)
-    .then(meal => !meal.rows[0] ? res.sendStatus(404) :
-      Food.find(req.params.id)
-        .then(food => !food ? res.sendStatus(404) :
-          MealFood.create(req.params)
-            .then(() => Promise.all(findMealAndFood(req))
-              .then(info => {
-                if (info[0].rows != null) {
-                  res.status(201).json(createMessage(
-                    info[0].rows[0].name,
-                    info[1].name,
-                    'create'
-                  ));
-                }
-              })
-            )
-        )
-    );
+    .then(meal => !meal.rows[0] ? res.sendStatus(404) : mealName = meal.rows[0].name);
+  Food.find(req.params.id)
+    .then(food => !food ? res.sendStatus(404) : foodName = food.name);
+
+  MealFood.create(req.params)
+    .then(() => res.status(201).json(createMessage(mealName, foodName, 'create')));
 };
 
 const destroy = (req, res, next) => {
